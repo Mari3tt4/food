fb92d0717b7dd12916a74cc21cac8e08c76334db
Marietta <125894364+Mari3tt4@users.noreply.github.com>
2023-09-30 13:45:09 +0700

    Add files via upload

---
+++ food/js/js.js
@@ -0,0 +1,15 @@
+function showProductPreview(id) {
+    var productPreview = document.getElementById(id);
+    productPreview.style.display = "block";
+}
+
+// Function to close the product preview
+function closeProductPreview(id) {
+    var productPreview = document.getElementById(id);
+    productPreview.style.display = "none";
+}
+
+// Attach click event handlers for close buttons
+document.getElementById("closePreview").addEventListener("click", function() {
+    closeProductPreview("susi");
+});
